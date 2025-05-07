34ed4db3abc081b58908863d4e2d9dc42937a92c
Shubham Sahu <sahushubham2208@gmail.com>
2025-05-07 15:39:09 +0530

    Selenium 07-05-2025 (ii)

--- 1st_code.py
+++ 1st_code.py
@@ -1,3 +1,5 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By  # <-- Import this
@@ -8,3 +10,7 @@ driver.get("https://rahulshettyacademy.com/AutomationPractice/")
 driver.maximize_window()
 
 driver.find_element(By.NAME, "enter-name").send_keys("Shubham Sahu")
+driver.find_element(By.ID, "confirmbtn").click()
+
+time.sleep(10)
+
