cd793dcb6fce360c5f4a02eb203714958af7a6df
Shubham Sahu <sahushubham2208@gmail.com>
2025-05-07 15:22:01 +0530

    Selenium 07-05-2025

---
+++ 1st_code.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By  # <-- Import this
+
+service = Service(r'C:\Users\Shubham Sahu\Desktop\chromedriver-win64\chromedriver.exe')
+driver = webdriver.Chrome(service=service)
+driver.get("https://rahulshettyacademy.com/AutomationPractice/")
+driver.maximize_window()
+
+driver.find_element(By.NAME, "enter-name").send_keys("Shubham Sahu")
